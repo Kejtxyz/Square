5417bbec7db6c38d246c23a88c28292e9d3b9213
MikiKru <michal_kruczkowski@o2.pl>
2019-03-31 10:34:36 +0200

    solution

--- src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
+++ src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
@@ -4,7 +4,34 @@ public class Pattern {
 
 	public String prepareSquareWithDiagonals(char sign, int size) {
 		String pattern = "";
-		// TO DO
+		// pętla iterująca po wierszach
+		for(int i = 0; i < size; i++){
+			// pętle iterująca po komórkach
+			for(int j = 0; j < size; j++){
+				if(i == 0 || i == size -1) {
+					if (j != size - 1) {
+						pattern = pattern + sign + " ";
+					} else {
+						pattern = pattern + sign + "\n";
+					}
+				} else if(i != 0){
+					if(j == 0 && i != size -1){
+						pattern = pattern + sign + " ";
+					} else if (i == j || i == size - j -1){
+						pattern = pattern + sign + " ";
+					} else if (j == 0 && i == size -1 || j == size -1){
+						pattern = pattern + sign + "\n";
+					} else {
+						pattern = pattern + "  ";
+					}
+				}
+			}
+		}
+		System.out.println(pattern);
 		return pattern;
+	}
+
+	public static void main(String[] args) {
+		new Pattern().prepareSquareWithDiagonals('#',7);
 	}
 }
