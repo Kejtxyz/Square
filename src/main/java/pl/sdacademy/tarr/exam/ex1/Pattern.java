af14aa48d784696ff97057400d7895b274213937
MikiKru <michal_kruczkowski@o2.pl>
2019-03-31 10:42:40 +0200

    solution with comments

--- src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
+++ src/main/java/pl/sdacademy/tarr/exam/ex1/Pattern.java
@@ -8,20 +8,20 @@ public class Pattern {
 		for(int i = 0; i < size; i++){
 			// pętle iterująca po komórkach
 			for(int j = 0; j < size; j++){
-				if(i == 0 || i == size -1) {
-					if (j != size - 1) {
+				if(i == 0 || i == size -1) {							// krawędzie górna dolna
+					if (j != size - 1) {								// wszystkie komórki poza ostatnią
 						pattern = pattern + sign + " ";
 					} else {
-						pattern = pattern + sign + "\n";
+						pattern = pattern + sign + "\n";				// ostatnia komórka
 					}
-				} else if(i != 0){
-					if(j == 0 && i != size -1){
+				} else if(i != 0){										// środek
+					if(j == 0){											// krawędź boczna lewa
 						pattern = pattern + sign + " ";
-					} else if (i == j || i == size - j -1){
+					} else if (i == j || i == size - j -1){				// przekątne
 						pattern = pattern + sign + " ";
-					} else if (j == 0 && i == size -1 || j == size -1){
+					} else if (j == 0 && i == size -1 || j == size -1){	// krawędz boczna prawa
 						pattern = pattern + sign + "\n";
-					} else {
+					} else {											// wypełnienie puste
 						pattern = pattern + "  ";
 					}
 				}
